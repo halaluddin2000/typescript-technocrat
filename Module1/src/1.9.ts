ff56e4824c790e6428aa45a4bb9d388f4a9655e6
halaluddin2000 <helaluddin7001@gmail.com>
2025-05-03 17:18:39 +0600

    type allias

---
+++ Module1/src/1.9.ts
@@ -0,0 +1,23 @@
+{
+  //
+  type Student = {
+    name: string;
+    contactNo: string;
+    gender: string;
+    address: string;
+  };
+  const student1: Student = {
+    name: "helal",
+    contactNo: "0181000000",
+    gender: "male",
+    address: "ctg",
+  };
+  const student2: Student = {
+    name: "belal",
+    contactNo: "0171000000",
+    gender: "male",
+    address: "ctg",
+  };
+
+  //
+}

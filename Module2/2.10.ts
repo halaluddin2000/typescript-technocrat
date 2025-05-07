0a77b0cf5f5e6457c17111650e70c18851ece9a0
halaluddin2000 <helaluddin7001@gmail.com>
2025-05-07 11:47:23 +0600

    mapping type

---
+++ Module2/2.10.ts
@@ -0,0 +1,23 @@
+{
+  //mapped type
+
+  const arrOfNumber: number[] = [1, 4, 5];
+
+  const arrOfString: string[] = arrOfNumber.map((number) => number.toString());
+  console.log(arrOfString);
+
+  type AreaNumber = {
+    height: number;
+    width: number;
+  };
+  type AreaString<T> = {
+    [key in keyof T]: T[key];
+  };
+
+  const area1: AreaString<{ height: string; width: number }> = {
+    height: "100",
+    width: 50,
+  };
+
+  //------------------------
+}

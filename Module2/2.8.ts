a942a0dc51db79661f8f8f80b66ff1c37b9c1d2f
halaluddin2000 <helaluddin7001@gmail.com>
2025-05-07 10:43:35 +0600

    simulate or promise difined

--- Module2/2.8.ts
+++ Module2/2.8.ts
@@ -3,9 +3,29 @@
 
   //--------simulate
 
-  const createPromise = () => {
-    return new Promise((resolve, reject) => {
-      const data: string = "something";
+  type todo = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+
+  const getTodo = async (): Promise<todo> => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+  };
+  getTodo();
+
+  type Something = { something: string };
+
+  const createPromise = (): Promise<Something> => {
+    return new Promise<Something>((resolve, reject) => {
+      const data: Something = { something: "something" };
       if (data) {
         resolve(data);
       } else {
@@ -13,6 +33,12 @@
       }
     });
   };
+  //calling create
+  const showData = async (): Promise<Something> => {
+    const data = await createPromise();
+    return data;
+  };
+  showData();
 
   //---------------
 }

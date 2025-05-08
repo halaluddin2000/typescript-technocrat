bfd6454e48577e4208d92dfe91ca2d3952de79e2
halaluddin2000 <helaluddin7001@gmail.com>
2025-05-08 23:59:58 +0600

    instanceof guard

---
+++ module3/3.4.ts
@@ -0,0 +1,17 @@
+{
+  //instanceof guard
+  class Animal {
+    name: string;
+    species: string;
+
+    constructor(name: string, species: string) {
+      this.name = name;
+      this.species = species;
+    }
+    makeSound() {
+      console.log("I am making sound");
+    }
+  }
+
+  //----------------
+}

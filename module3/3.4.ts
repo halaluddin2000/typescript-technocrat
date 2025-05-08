ffb2638b6ecc2637296b1c80af06968951db9eb4
halaluddin2000 <helaluddin7001@gmail.com>
2025-05-09 00:41:45 +0600

    instanceof

--- module3/3.4.ts
+++ module3/3.4.ts
@@ -13,5 +13,45 @@
     }
   }
 
+  class Dog extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+    makeBark() {
+      console.log("I am barking");
+    }
+  }
+
+  class Cat extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+    makeMeaw() {
+      console.log("I am Mewaing");
+    }
+  }
+  //smart way te kaj korte hobe
+  const isDog = (animal: Animal): animal is Dog => {
+    return animal instanceof Dog;
+  };
+  const isCat = (animal: Animal): animal is Cat => {
+    return animal instanceof Dog;
+  };
+
+  const getAnimal = (animal: Animal) => {
+    if (isDog(animal)) {
+      animal.makeBark();
+    } else if (isCat(animal)) {
+      animal.makeMeaw();
+    } else {
+      animal.makeSound();
+    }
+  };
+
+  const dog = new Dog("Dog Bhai", "dog");
+  const cat = new Dog("Cat Bhai", "cat");
+
+  getAnimal(cat);
+
   //----------------
 }

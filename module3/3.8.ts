ef0614a81a4cc54d66d11d85ae20f6a211ff55f0
halaluddin2000 <helaluddin7001@gmail.com>
2025-05-10 04:22:13 +0600

    shape or circle and reactangle

--- module3/3.8.ts
+++ module3/3.8.ts
@@ -28,21 +28,49 @@
 
   getSleepingHours(person3);
 
-
-  class Shape{
-    getArea(): number{
-        return 0
+  class Shape {
+    getArea(): number {
+      return 0;
     }
   }
 
   //pi*r*r
-  class Circle extends Shape{
+  class Circle extends Shape {
+    radius: number;
+    constructor(radius: number) {
+      super();
+      this.radius = radius;
+    }
     getArea(): number {
-        (radius: number): number{
-            
-        }
+      return Math.PI * this.radius * this.radius;
     }
   }
+  //Reactangle
+  //height * Width
+  class Reactangle extends Shape {
+    height: number;
+    width: number;
+    constructor(height: number, width: number) {
+      super();
+      this.height = height;
+      this.width = width;
+    }
+    getArea(): number {
+      return this.height * this.width;
+    }
+  }
+
+  const getShapeArea = (param: Shape) => {
+    console.log(param.getArea());
+  };
+
+  const shape1 = new Shape();
+  const shape2 = new Circle(10);
+  const shape3 = new Reactangle(10, 20);
+
+  getShapeArea(shape1);
+  getShapeArea(shape2);
+  getShapeArea(shape3);
 
   //-------------
 }
